c34e5b5bf6d398ec8b9d13a10d8e074421ac59a8
Ayushi Agrawal <ayushiverma19a@gmail.com>
2022-12-30 21:09:30 +0530

    Update gitignore file

--- src/App.java
+++ src/App.java
@@ -1,12 +1,8 @@
-import binarytree.basicfunctions.Postorder;
-import binarytree.simplecode.FindMaximum;
-import binarytree.simplecode.FindMinimum;
 import binarytree.simplecode.FindSum;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Started ...");
-
 
         FindSum sum = new FindSum();
         int ans = sum.execute();
